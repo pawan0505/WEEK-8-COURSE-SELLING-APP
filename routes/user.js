f395f14fde596e47a4dbdc7530e2767376466fbf
Pawan Kumar <104243843+pawan0505@users.noreply.github.com>
2025-01-17 02:04:21 +0530

    step 5 - Added admin routes

--- routes/user.js
+++ routes/user.js
@@ -2,23 +2,23 @@ const { Router } = require("express");
 
 const userRouter = Router();
 
-    userRouter.post("/signup", function(req, res) {
-        res.json({
-            message: "signup endpoint"
-        })
+userRouter.post("/signup", function(req, res) {
+    res.json({
+        message: "signup endpoint"
     })
-    
-    userRouter.post("/signin", function(req, res){
-        res.json({
-            message: "signup endpoint"
-        })
+})
+
+userRouter.post("/signin", function(req, res){
+    res.json({
+        message: "signup endpoint"
     })
-    
-    userRouter.get("/purchases", function(req, res){
-        res.json({
-            message: "signup endpoint"
-        })
+})
+
+userRouter.get("/purchases", function(req, res){
+    res.json({
+        message: "signup endpoint"
     })
+})
 
 
 module.exports = {
