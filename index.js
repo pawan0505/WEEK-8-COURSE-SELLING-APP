f5e5d27ed821491337c921b0476400b586457213
Pawan Kumar <104243843+pawan0505@users.noreply.github.com>
2025-01-17 01:51:06 +0530

    step 4 - added course router

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const { userRouter } = require("./routes/user");
 const { courseRouter } = require("./routes/course");
 const app = express();
-app.use(express.json());
+// app.use(express.json());
 
 app.use("/user", userRouter);
 app.use("/course", courseRouter);
