2ddc07ec673073fdac735b17733afc2eef6064cd
Pawan Kumar <104243843+pawan0505@users.noreply.github.com>
2025-01-16 23:41:12 +0530

    added skeleton of user routes

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-console.log("hello world");
+const express = require("express");
+
+const app = express();
+app.use(express.json());
+
+app.post("/user/signup", function(req, res) {
+
+})
+
+app.post("/user/signin", function(req, res){
+
+})
+
+app.get("/user/purchases", function(req, res){
+
+})
+
+app.post("/course/purchase", function(req, res){
+
+})
+
+app.get("/courses", function(req, res){
+
+})
+
+
+
+app.listen(3000, () => {
+    console.log("server is running on port 3000");
+})
